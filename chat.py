5ada1ebcca79117b1deff40eb9f19ba06f43eb54
koo5 <kolman.jindrich@gmail.com>
2021-05-01 05:50:08 +0200

    json hack

--- chat.py
+++ chat.py
@@ -1,6 +1,6 @@
 import asyncio
 import datetime
-
+import json
 import aiohttp
 from aiohttp import web
 
@@ -36,6 +36,13 @@ async def chat_page(request):
             content_type='text/html')
 
 
+async def snd(ws,x):
+    if isinstance(x, str):
+        await ws.send_str(x)
+    else:
+        await ws.send_json(x)
+
+
 @routes.get('/{name}/ws/')
 async def websocket(request):
     name = request.match_info['name']
@@ -44,13 +51,14 @@ async def websocket(request):
 
     await send_to_all('system', f'{name} joined!')
     for text in list(history):
-        await ws.send_str(text)
+        await snd(ws,text)
     websockets.add(ws)
 
     try:
         async for msg in ws:
-            if msg.type == aiohttp.WSMsgType.TEXT:
-                await send_to_all(name, msg.data)
+            if msg.type in [aiohttp.WSMsgType.TEXT]:
+                await send_to_all_json(json.loads(msg.data))
+                #await send_to_all(name, msg.data)
             elif msg.type == aiohttp.WSMsgType.ERROR:
                 print('ws connection closed with exception %s' %
                     ws.exception())
@@ -65,7 +73,9 @@ async def websocket(request):
 async def send_to_all(name, message):
     now = datetime.datetime.now()
     text = f'{now:%H:%M:%S} – {name}: {message}'
+    await send_to_all_raw(text)
 
+async def send_to_all_raw(text):
     history.append(text)
     if len(history) > 20:
         del history[:10]
@@ -73,6 +83,17 @@ async def send_to_all(name, message):
     tasks = set()
     for ws in websockets:
         tasks.add(asyncio.create_task(ws.send_str(text)))
+    while tasks:
+        done, tasks = await asyncio.wait(tasks)
+
+async def send_to_all_json(text):
+    history.append(text)
+    if len(history) > 20:
+        del history[:10]
+
+    tasks = set()
+    for ws in websockets:
+        tasks.add(asyncio.create_task(ws.send_json(text)))
     while tasks:
         done, tasks = await asyncio.wait(tasks)
 
---
+++ send_json.py
@@ -0,0 +1,12 @@
+import asyncio
+import sys
+import json
+import aiohttp
+
+
+async def send_one(msg):
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect('http://localhost:8080/CLI/ws/') as ws:
+            await ws.send_json(json.loads(msg))
+
+asyncio.run(send_one(' '.join(sys.argv[1:])))
